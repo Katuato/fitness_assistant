7a70cb36e9fb3d07a22dbb0fc5b9e037210645c8
Katuato <117164622+Katuato@users.noreply.github.com>
2025-11-24 15:23:13 +0300

    Folders

---
+++ fitness_assistant/fitness_assistant/fitness_assistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitness_assistantApp.swift
+//  fitness_assistant
+//
+//  Created by katuato on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct fitness_assistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
