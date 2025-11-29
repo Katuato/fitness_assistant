b32b2900bc70baf0725592af2168d3559a69df5b
Erik Ishbaev <ishbaev.erik@gmail.com>
2025-11-29 04:04:53 +0300

    feat(services): add service layer

---
+++ Backend/fitness_assistant/services/__init__.py
@@ -0,0 +1,13 @@
+from fitness_assistant.services.user_service import UserService
+from fitness_assistant.services.exercise_service import ExerciseService
+from fitness_assistant.services.session_service import SessionService
+from fitness_assistant.services.media_service import MediaService
+from fitness_assistant.services.analysis_service import AnalysisService
+
+__all__ = [
+    "UserService",
+    "ExerciseService",
+    "SessionService",
+    "MediaService",
+    "AnalysisService",
+]
