52e3bc97c3749ad7acb666d021f18707e44f7e28
Erik Ishbaev <ishbaev.erik@gmail.com>
2025-12-14 15:51:25 +0300

    feat(settings): update python version and add libs for work with JWT and hash

--- Backend/fitness_assistant/config.py
+++ Backend/fitness_assistant/config.py
@@ -15,6 +15,12 @@ class Settings(BaseSettings):
     # Debug mode
     debug: bool = False
 
+    # JWT Settings
+    jwt_secret_key: str = "52A5285215296C067EDF6F8A052F52152"
+    jwt_algorithm: str = "HS256"
+    jwt_access_token_expire_minutes: int = 20  
+    jwt_refresh_token_expire_days: int = 7  
+
 
 @lru_cache
 def get_settings() -> Settings:
